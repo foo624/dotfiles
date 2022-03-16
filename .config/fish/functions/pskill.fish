d69999d588541e32274a351143d44cf25e61b52e
foo624 <63292756+foo624@users.noreply.github.com>
2022-03-16 21:54:54 +0900

    Modified configuration.

--- .config/fish/functions/pskill.fish
+++ .config/fish/functions/pskill.fish
@@ -1,14 +1,14 @@
 function pskill -d "Fuzzy-find and ps and kill"
-  set -l kill_signal ""
+  set -l kill_signal
   if test -n "$argv[1]"
     set kill_signal "-"$argv[1]
   end
 
-  set -l FZF_CMD_OPTS "--layout=reverse --height 100% --header-lines=1"
-  ps u | eval fzf $FZF_CMD_OPTS | awk '{print $2}' | read -l result
+  set -l FZF_CMD_OPTS "--layout=reverse --height 100% --multi --header-lines=1"
+  set -l results (ps ux | eval fzf $FZF_CMD_OPTS | awk '{print $2}')
 
-  if test -n "$result"
-    echo kill "$kill_signal" "$result"
-    kill -"$kill_signal" "$result"
+  if test -n "$results"
+    echo kill $kill_signal $results
+    kill $kill_signal $results
   end
 end
